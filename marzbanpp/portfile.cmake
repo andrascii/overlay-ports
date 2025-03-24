7754dec417b18db45685a3ab3627eaa6cdf90b37
Andrey <apugachevdev@gmail.com>
2025-03-25 02:19:04 +0300

    marzbanpp portfile

---
+++ marzbanpp/portfile.cmake
@@ -0,0 +1,22 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO andrascii/marzbanpp
+    REF "${VERSION}"
+    SHA512 6cb410f7e451bf2cde5c13cfafdf1ac3256f0e2943afa26132acadbc87ee9847abc0260617af9e53d020205af776deb4860b3766c6a39761834ac381f2907114
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")

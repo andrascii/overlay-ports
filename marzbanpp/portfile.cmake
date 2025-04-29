ad160b7225f434afcb78db2ea90edc11b57ab8dd
Andrey <apugachevdev@gmail.com>
2025-04-29 19:22:57 +0300

    Update marzbanpp library version

--- marzbanpp/portfile.cmake
+++ marzbanpp/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO andrascii/marzbanpp
-    REF "1.0.2"
-    SHA512 a369b3fc53cf7d27a8b1eb3777d82506e59d561b36f056716e90ae41eaba9a7ede83634c7c2530ab459a70db08b607fa2dba8cdf99ec63edc0aaa10695b4a78d
+    REF "1.0.3"
+    SHA512 ef50079cfeaeda2cdc0e6eee0d4133663fc2cacb9eb626034c7775e4dff7feed9e3144c47ab9e1426ab2e03e4f2cd2a07eb9bcfcfd804ff0427798f0d16d6dc6
     HEAD_REF main
 )
 
